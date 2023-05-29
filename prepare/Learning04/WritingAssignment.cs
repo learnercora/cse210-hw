fbe940e353b083eae6c7252d9392a90441db0cbb
coralin <zxc188950@yahoo.com.tw>
2023-05-29 16:20:24 +0800

    Inheritance Learning Activity

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    
+
+    public WritingAssignment(string studentName, string topic, string title) 
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // return $"{_title} by {_studentName}"; //set "protected"
+        //return $"{_title} by {base.GetStudentName()}";
+
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+
+}
